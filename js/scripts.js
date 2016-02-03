c360768ea17ad00f2f9a7e03c00782b0c16fa3a1
Kyle Langley <vawx@Kyles-MacBook-Air.local>
2016-02-03 14:13:28 -0800

    creates crypto square

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
+String.prototype.noPunc = function( ) { 
+    return this.replace(/[.,\/#!'$%\^&\*;:{}=\-_`~()]/g,"");
+}
 
 String.prototype.crypto = function( ) {
-    var noPunc = this.replace(/[.,\/#!'$%\^&\*;:{}=\-_`~()]/g,"").split("");
-    var row = Math.round( Math.sqrt(noPunc.length) );
+    var noPunc = this.split("");
+    var row = Math.round( Math.sqrt(noPunc.length) ) + 1;
     var column = Math.round( noPunc.length / row );
 
     var sentences = [];
@@ -21,5 +24,83 @@ String.prototype.crypto = function( ) {
             sentence = "";
          }
     }
-    return sentences;
+    var groups = []
+    for( var i = 0; i < sentences.length; i++ )
+    {
+        groups.push( sentences[ i ] );   
+    }
+    
+    var trueCrypto = [];
+    var crypt = "";
+    var wordCount = groups[ 0 ].length;
+    var currentCount = 0;
+    
+    while( currentCount < wordCount )
+    {
+        for( var i = 0; i < groups.length; i++ )
+        {
+            var groupLetter = groups[ i ][ currentCount ];
+            if( groupLetter )
+            {
+                crypt += groups[ i ][ currentCount ];
+            }
+        }
+        crypt += " ";
+        trueCrypto.push( crypt );
+        crypt = "";
+        currentCount += 1;
+    }
+    
+    return trueCrypto;
 }
+
+$(document).ready(function( ) {
+    
+    function reset( )
+    {
+        $('#phrases > li').remove( ); 
+    }
+    
+    $("form#crypto").submit( function(event) {
+       event.preventDefault( );
+       
+       reset( );
+       
+       var userInput = $("#new-crypto").val( ).noPunc( );
+       var boxLayout = Math.round( Math.sqrt( userInput.length ) / 2 );
+       var cryptoInput = userInput.crypto( ).join( ).noPunc( );
+       
+       var line = "";
+       var wordCount = 0;
+       var lineCount = 0;
+       for( var i = 0; i < cryptoInput.length; i++ )
+       {
+           if ( cryptoInput[ i ] !== ' ' && cryptoInput[ i ] !== '' )
+           {
+                line += cryptoInput[ i ];
+                wordCount += 1;
+                if( wordCount >= boxLayout )
+                {
+                    wordCount = 0;
+                    lineCount += 1;
+                    if( lineCount === ( boxLayout - 1 ) )
+                    {
+                        $("ul#phrases").append("<li><span class='crypto' style='font-family:monospace,monospace;'>" + line + "</span></li>" ); 
+                        line = "";
+                        lineCount = 0;
+                    }
+                    else
+                    {
+                        line += " ";
+                    }
+                }
+           }
+       }
+       $("ul#phrases").append("<li><span class='crypto' style='font-family:monospace,monospace;'>" + line + "</span></li>" ); 
+    });
+});
+
+
+
+
+//$("ul#phrases").append("<li><span class='crypto' style='font-family:monospace,monospace;'>" + userInput[ i ] + "</span></li>" ); 
